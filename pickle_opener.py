a61fefcd1dd863fd33f8496ea3c6fc3f337e5b10
Alexander Rodionov <alx1dr@gmail.com>
2021-04-10 14:11:06 +0300

    1 

--- pickle_opener.py
+++ pickle_opener.py
@@ -8,4 +8,4 @@ with open(sys.argv[1], 'rb') as fd:
 EDITOR = '/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl'
 
 cmd = EDITOR, '--new-window'
-subprocess.run(cmd, input=repr(obj)[:1000], text=True)
+subprocess.run(cmd, input=repr(obj)[:100_000], text=True)
