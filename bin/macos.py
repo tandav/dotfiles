c47d8f2befedfae5321cb0ee288dbde742a02018
Alexander Rodionov <alx1dr@gmail.com>
2021-04-12 09:21:17 +0300

    add n_tabs function

--- bin/macos.py
+++ bin/macos.py
@@ -11,3 +11,15 @@ def notification(text, title=None, subtitle=None, sound=False):
         code += 'sound name "Glass"' # more sounds: ls /System/Library/Sounds
     cmd  = 'osascript', '-e', code
     subprocess.run(cmd)
+
+
+def n_tabs(browser='Google Chrome'):
+    js_code = f'''
+    let browser = Application({browser!r})
+    let n_tabs = 0
+    for (let i = 0; i < browser.windows.length; i++)
+        n_tabs += browser.windows[i].tabs.length
+    '''
+
+    cmd = 'osascript', '-l', 'JavaScript', '-e', js_code
+    return int(subprocess.check_output(cmd, text=True).strip())
