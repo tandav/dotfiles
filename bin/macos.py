8b7158a6f273ffd40c16c2a5529cdf047b2eda6d
Alexander Rodionov <alx1dr@gmail.com>
2021-05-03 21:46:34 +0300

    add windows, urls, titles to tabs functions

--- bin/macos.py
+++ bin/macos.py
@@ -1,4 +1,6 @@
 import subprocess
+import string
+import json
 
 
 def notification(text, title=None, subtitle=None, sound=False):
@@ -13,13 +15,43 @@ def notification(text, title=None, subtitle=None, sound=False):
     subprocess.run(cmd)
 
 
-def n_tabs(browser='Google Chrome'):
+def n_tabs(browser='Google Chrome', n_windows=False):
     js_code = f'''
     let browser = Application({browser!r})
     let n_tabs = 0
-    for (let i = 0; i < browser.windows.length; i++)
+    let n_windows = browser.windows.length
+    for (let i = 0; i < n_windows; i++)
         n_tabs += browser.windows[i].tabs.length
     '''
+    
+    # final value, returned to the caller
+    if n_windows:
+        js_code += 'out = [n_tabs, n_windows]\n'
+        cmd = 'osascript', '-l', 'JavaScript', '-e', js_code
+        return [int(x) for x in  subprocess.check_output(cmd, text=True).strip().split(', ')]
+        
+    else:
+        js_code += 'out = n_tabs\n'
+        cmd = 'osascript', '-l', 'JavaScript', '-e', js_code
+        return int(subprocess.check_output(cmd, text=True).strip())
 
+
+def tabs(browser='Google Chrome'):
+    js_code = string.Template('''
+    let browser = Application('Google Chrome')
+    let tabs = []
+    for (let i = 0; i < browser.windows.length; i++) {
+        let window = []
+        for (let j = 0; j < browser.windows[i].tabs.length; j++) {
+            let tab_name = browser.windows[i].tabs[j].name()
+            let tab_url  = browser.windows[i].tabs[j].url()
+            window.push([tab_name, tab_url])
+        }
+        tabs.push(window)
+    }
+    out = JSON.stringify(tabs)
+    ''').substitute(browser=browser)
+    
     cmd = 'osascript', '-l', 'JavaScript', '-e', js_code
-    return int(subprocess.check_output(cmd, text=True).strip())
+    tabs = json.loads(subprocess.check_output(cmd, text=True))
+    return tabs
